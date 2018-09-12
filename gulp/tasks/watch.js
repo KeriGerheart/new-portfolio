cc0130fbce093b75cc3c0bf8251a0eb15fbc68a9
Keri <kerigerheart@gmail.com>
2018-09-12 15:37:34 -0400

    added general styling for page

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+broswerSync = require('browser-sync').create();
+
+
+gulp.task('watch', function () {
+
+  broswerSync.init({
+    notify: false,
+    server: {
+      baseDir: "site"
+    }
+  });
+
+  watch('./site/index.html', function() {
+    broswerSync.reload();
+  });
+
+  watch('./site/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./site/temp/styles/styles.css')
+    .pipe(broswerSync.stream());
+});
